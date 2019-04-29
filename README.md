be47276c8266b3dc7c9549f354f9405c0830b0df
yawkale <yakov@uteam.co.il>
2019-04-29 13:28:10 +0300

    Dev8 (#5)
    * add v7 source
    
    * add test project
    
    * fix 7.5
    
    * fix for 7.5
    
    * fix test project
    
    * add v8 support
    
    * fix verions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # missingcode-umbraco-html-minifier
-The Web Markup Minifier  for Umbraco
+The Web Markup Minifier for Umbraco, remove white space and compress HTML for better SEO rank.
+
+Enable Html Minifier by change debug="false" in web.config
+
+
+
+Settings:
+
+RemoveAttributeQuotes 
+
+RemoveOptionalEndTags
---
+++ src/MissingCode.Umbraco.HtmlMinifier/PluginSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace MissingCode.Umbraco.HtmlMinifier
+{
+    public static class PluginSettings
+    {
+
+        private static T GetProperty<T>(string key, T defaultValue)
+        {
+
+            var configuration = ConfigurationManager.AppSettings[key];
+
+            if (string.IsNullOrEmpty(configuration))
+                return defaultValue;
+
+            return (T)Convert.ChangeType(configuration, typeof(T), CultureInfo.InvariantCulture);
+
+        }
+
+        public static bool RemoveAttributeQuotes => GetProperty<bool>("RemoveAttributeQuotes", false);
+        public static bool RemoveOptionalEndTags => GetProperty<bool>("RemoveOptionalEndTags", false);
+
+    }
+}
