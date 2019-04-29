11ed168fc8855d2314ce336b74e21ac735d0bb1c
yawkale <yakov@uteam.co.il>
2019-04-29 12:46:31 +0300

    fix verions

--- src/MissingCode.Umbraco.HtmlMinifier/Properties/AssemblyInfo.cs
+++ src/MissingCode.Umbraco.HtmlMinifier/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MissingCode.Umbraco.HtmlMinifier")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Web Markup Minifier for Umbraco")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dyfort")]
 [assembly: AssemblyProduct("MissingCode.Umbraco.HtmlMinifier")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.5.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("0.9.5.0")]
