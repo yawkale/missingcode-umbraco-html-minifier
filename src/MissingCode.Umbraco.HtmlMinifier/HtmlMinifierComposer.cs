7bc3c5000cdf28e393edd5aeba40f265a0f3e13f
Yakov Lebski <yawkale@gmail.com>
2022-04-28 13:23:53 -0400

    support umbraco 9

---
+++ src/MissingCode.Umbraco.HtmlMinifier/HtmlMinifierComposer.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using WebMarkupMin.AspNetCore5;
+using WebMarkupMin.Core;
+
+namespace MissingCode.Umbraco.HtmlMinifier
+{
+    public class StartupFilter : IStartupFilter
+	{
+		public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) => app =>
+		{
+			app.UseWebMarkupMin();
+			next(app);
+		};
+	}
+
+	public class HtmlMinifierComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+			builder.Services.AddTransient<IStartupFilter, StartupFilter>();
+
+			var htmlMinifierOptions = new HtmlMinifierPluginSettings();
+			builder.Config.GetSection(HtmlMinifierPluginSettings.ConfigurationName).Bind(htmlMinifierOptions);
+
+
+			builder.Services.AddWebMarkupMin(options =>
+			{
+				options.AllowMinificationInDevelopmentEnvironment = htmlMinifierOptions.AllowMinificationInDevelopmentEnvironment;
+				options.AllowCompressionInDevelopmentEnvironment = htmlMinifierOptions.AllowMinificationInDevelopmentEnvironment;
+			})
+				.AddHtmlMinification(options =>
+				{
+					HtmlMinificationSettings settings = options.MinificationSettings;
+					settings.RemoveRedundantAttributes = true;
+					settings.RemoveHttpProtocolFromAttributes = true;
+					settings.RemoveHttpsProtocolFromAttributes = true;
+
+					if (!htmlMinifierOptions.RemoveAttributeQuotes)
+                    {
+						settings.AttributeQuotesRemovalMode = HtmlAttributeQuotesRemovalMode.KeepQuotes;
+					}					
+					settings.RemoveOptionalEndTags = htmlMinifierOptions.RemoveOptionalEndTags;
+				});
+
+
+			
+
+		}
+    }
+}
---
+++ src/MissingCode.Umbraco.HtmlMinifier/HtmlMinifierPluginSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace MissingCode.Umbraco.HtmlMinifier
+{
+    public class HtmlMinifierPluginSettings
+    {
+
+        public const string ConfigurationName = "HtmlMinifier";
+
+        [DefaultValue(false)]
+        public bool RemoveAttributeQuotes { get; set; } = false;
+
+        [DefaultValue(false)]
+        public bool RemoveOptionalEndTags { get; set; } = false;
+
+        [DefaultValue(false)]
+        public bool AllowMinificationInDevelopmentEnvironment { get; set; } = false;
+
+        
+
+    }
+}
